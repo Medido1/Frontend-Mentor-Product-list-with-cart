cd8add80d8be85416c6f08b035368dcd59f03386
Medido1 <medi1222@gmail.com>
2024-08-02 07:48:36 +0100

    Add confirm order button

--- src/components/YourCart.jsx
+++ src/components/YourCart.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import emptyCart from '../assets/images/illustration-empty-cart.svg';
 import removeBtn from '../assets/images/icon-remove-item.svg';
 import carbonIcon from '../assets/images/icon-carbon-neutral.svg';
 import '../styles/YourCart.css';
 
 export default function YourCart({TotalCount, myData}) {
-
+  const [isConfirmed, setIsConfirmed] = useState(false);
+ 
   function calcuateTotalPrice() {
     return myData.reduce((total, item) => {
       return total + (item.number * item.price);
@@ -52,6 +53,7 @@ export default function YourCart({TotalCount, myData}) {
             <img src={carbonIcon} className='icon'/>
             <p>this is a <b>carbon-netural</b> delivery</p>
           </div>
+          <button className="btn" onClick={() => setIsConfirmed(true)}>Confirm Order</button>
           </>
         }
       
