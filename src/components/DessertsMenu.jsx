56f68b9eb8ebfdc2850ed8d7d955f6811c441145
Medido1 <medi1222@gmail.com>
2024-08-16 09:36:16 +0100

    Improve DessertsMenu component by adding responsive behavior

--- src/components/DessertsMenu.jsx
+++ src/components/DessertsMenu.jsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/DessertsMenu.css';
 import Dessert from './Dessert';
 
 export default function DessertsMenu(
   {setTotalCount, myData, updateItemNumber, isConfirmed}
   ){
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 768)
 
+    useEffect(() => {
+      const handleResize = () => {
+        setIsMobile(window.innerWidth <= 768)
+      };
+      window.addEventListener('resize', handleResize)
+      return () => {
+        window.removeEventListener('resize', handleResize)
+      }
+    }, [])
   return (
     <ul className="desserts_menu">
-      {(!isConfirmed || window.innerWidth > 768) && myData.map((info) => {
-        return <Dessert key={info.name}
-          imgSrc={info.image.mobile} category={info.category}
-          dessertName={info.name} price={info.price}
-          setTotalCount={setTotalCount} updateItemNumber={updateItemNumber}
+      {(!isConfirmed || !isMobile) && myData.map((info) => {
+        return <Dessert 
+          key={info.name}
+          imgSrc={info.image.mobile} 
+          category={info.category}
+          dessertName={info.name} 
+          price={info.price}
+          setTotalCount={setTotalCount} 
+          updateItemNumber={updateItemNumber}
           itemCount={info.number}
           />
       })}
