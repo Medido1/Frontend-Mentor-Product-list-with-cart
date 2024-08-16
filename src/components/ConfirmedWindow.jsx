0ce56ac91e86c8d32a7ee664816b7e31e079b1cd
Medido1 <medi1222@gmail.com>
2024-08-16 10:04:57 +0100

    Improve code

--- src/components/ConfirmedWindow.jsx
+++ src/components/ConfirmedWindow.jsx
@@ -3,11 +3,11 @@ import confirmedIcon from '../assets/images/icon-order-confirmed.svg';
 import '../styles/ConfirmedWindow.css';
 import ConfirmedCartItems from './ConfirmedCartItems';
 
-export default function confirmedWindow({myData, calculateTotalPrice, setIsConfirmed, setTotalCount}) {
+export default function ConfirmedWindow({myData, calculateTotalPrice, setIsConfirmed, setTotalCount}) {
   return (
     <div className="confirmed_window">
       <header>
-        <img src={confirmedIcon} className='icon'/>
+        <img src={confirmedIcon} className='icon' alt='order confirmed icon'/>
         <h2>Order Confirmed</h2>
         <p>We hope you enjoy your food!</p>
       </header>
